7c63731fac1407422e194547fcbaa83f3adaa227
Namisi-Aaron <aaronnamisi@gmail.com>
2025-08-31 21:15:55 +0300

    Cache Property List View: properties/views.py, properties/urls.py, alx_backend_caching_property_listings/urls.py created

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+from properties.models import Property
 
-# Create your views here.
+@cache_page(60 * 15)
+def property_list(request):
+    properties = Property.objects.all()
+    data = {"properties": list(properties.values())}
+    return JsonResponse(data)
