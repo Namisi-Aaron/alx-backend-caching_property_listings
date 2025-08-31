0045dbae80bafb00392ade15bd427ef9f129947b
Namisi-Aaron <aaronnamisi@gmail.com>
2025-08-31 21:19:13 +0300

    Cache Property List View: properties/views.py modified

--- properties/views.py
+++ properties/views.py
@@ -6,6 +6,6 @@ from properties.models import Property
 def property_list(request):
     try:
         properties = Property.objects.all()
-        return JsonResponse({"properties": list(properties.values())})
+        return JsonResponse(data={"properties": list(properties.values())}, status=200)
     except Exception as e:
-        return JsonResponse({"error": str(e)}, status=500)
+        return JsonResponse(data={"error": str(e)}, status=500)
